5bcf0e3775a579bbf91901c1b34b3fda90ecaf1e
Salma Elshahawy <salmaeng71@gmail.com>
2020-04-03 19:08:27 -0400

    backward elimination automation

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/m_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/m_regression.py
@@ -48,8 +48,56 @@ y_pred = regressor.predict(X_test)
 
 X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
 # start backword elimination 
-X_opt = X[:, [0,1,2,3,4,5]]
+#X_opt = X[:, [0,1,2,3,4,5]]
+"""
+def backwardElimination(x, sl):
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = np.delete(x, j, 1)
+    regressor_OLS.summary()
+    return x
+ 
+SL = 0.05
+X_opt = np.array(X[:, [0, 1, 2, 3, 4, 5]], dtype = float)
+X_Modeled = backwardElimination(X_opt, SL)
 
+print(X_Modeled)
+"""
+
+def backwardElimination(x, SL):
+    numVars = len(x[0])
+    temp = np.zeros((50,6)).astype(int)
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        adjR_before = regressor_OLS.rsquared_adj.astype(float)
+        if maxVar > SL:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    temp[:,j] = x[:, j]
+                    x = np.delete(x, j, 1)
+                    tmp_regressor = sm.OLS(y, x).fit()
+                    adjR_after = tmp_regressor.rsquared_adj.astype(float)
+                    if (adjR_before >= adjR_after):
+                        x_rollback = np.hstack((x, temp[:,[0,j]]))
+                        x_rollback = np.delete(x_rollback, j, 1)
+                        print (regressor_OLS.summary())
+                        return x_rollback
+                    else:
+                        continue
+    regressor_OLS.summary()
+    return x
+ 
+SL = 0.05
+X_opt = np.array(X[:, [0, 1, 2, 3, 4, 5]], dtype = float)
+X_Modeled = backwardElimination(X_opt, SL)
+
+"""
 X_opt = np.array(X[:, [0,1,2,3,4,5]], dtype = float)
 
 model = sm.OLS(endog = y, exog = X_opt).fit()
@@ -89,3 +137,4 @@ model = sm.OLS(endog = y, exog = X_opt).fit()
 
 model.summary()
 
+"""
